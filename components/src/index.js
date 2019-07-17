8c02a6df8ca419847a5e4008a21e02b429c04f63
Elmae <manfersr@hotmail.com>
2019-07-17 00:04:07 -0500

    Created CommentDetail Component

---
+++ components/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('#root'))
