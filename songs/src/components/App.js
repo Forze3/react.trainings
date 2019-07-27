28243f2f0fda2bdd9121fb1fa115ba0a61d38d9c
Elmae <manfersr@hotmail.com>
2019-07-26 21:07:58 -0500

    Implemented react-redux base

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import SongList from './SongList'
 
-const App =() =>{
-    return <div>App</div>;
+const App = () =>{
+    return (
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="ui column eight wide">
+                    <SongList />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
---
+++ songs/src/components/SongList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
+class SongList extends Component{
+    renderList(){
+        return this.props.songs.map((song)=>{
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button 
+                            className="ui primary button"
+                            onClick={() => this.props.selectSong(song)}
+                        >
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+    
+    render (){
+        
+        return <div className="ui divided list">{this.renderList()}</div>;
+    }
+}
+
+const mapStateToProps = state =>{
+    console.log(state);
+    return{ songs: state.songs };
+};
+
+export default connect(
+    mapStateToProps, 
+    { selectSong }
+)(SongList);
