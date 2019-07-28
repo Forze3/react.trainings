88470c142442922db5eafe1e2f66ef42d52c1586
Elmae <manfersr@hotmail.com>
2019-07-27 21:24:07 -0500

    Applied redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,8 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
-export const fetchPost = async () => {
-    const response = await jsonPlaceHolder.get('/posts');
-    
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    };
+export const fetchPost = () => async dispatch => {
+        const response =  await jsonPlaceHolder.get('/posts');
+        
+        dispatch({ type: 'FETCH_POSTS', payload: response });
 };
+ 
