0b0f94ebdc22b5748dda3027114a38e9c9c13df4
Elmae <manfersr@hotmail.com>
2019-08-17 21:12:34 -0500

    Refactored UserHeader component

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsReducer';
 import usersReducer from './usersReducer';
+
 export default combineReducers({
     posts: postsReducer,
     users: usersReducer
