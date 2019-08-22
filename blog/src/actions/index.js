f5f7a90cb2afada52a9b1b9c44b363344ed09d75
Elmae <manfersr@hotmail.com>
2019-08-21 21:56:01 -0500

    Refactored action creator fetchPostsAndUsers

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,9 +3,12 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
         await  dispatch(fetchPost());
-        
-        const userIds = _.uniq(_.map(getState().posts, 'userId'));
-        userIds.forEach(id => dispatch(fetchUser(id)));
+
+        _.chain(getState().posts)
+                .map('userId')
+                .uniq()
+                .forEach(id => dispatch(fetchUser(id)))
+                .value();
 };
 
 export const fetchPost = () => async dispatch => {
