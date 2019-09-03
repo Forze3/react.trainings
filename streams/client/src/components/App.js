6c60023b297f8926a39a5af5b68856701c30a25c
Elmae <manfersr@hotmail.com>
2019-09-03 09:21:44 -0500

    Created stream components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
 const App = () => {
-    return <div>App</div>;
+    return( 
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact componet={StreamList} />
+            <Route path="/streams/new" exact componet={StreamCreate} />
+            <Route path="/streams/edit" exact componet={StreamEdit} />
+            <Route path="/streams/delete" exact componet={StreamDelete} />
+            <Route path="/streams/show" exact componet={StreamShow} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
 };
 
 export default App;
