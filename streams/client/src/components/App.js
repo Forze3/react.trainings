be76c5a77748faa956d647cefbc3459103dc7458
Elmae <manfersr@hotmail.com>
2019-09-05 08:25:01 -0500

    Added path for the components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import StreamCreate from './streams/SreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
 const App = () => {
-    return <div>App</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
 };
 
 export default App;
