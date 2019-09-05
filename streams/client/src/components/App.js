210287f319dcc0e084b1d3e8895b05a560f55dfd
Elmae <manfersr@hotmail.com>
2019-09-05 08:40:08 -0500

    Created an linked header component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import StreamCreate from './streams/SreamCreate';
 import StreamDelete from './streams/StreamDelete';
@@ -7,11 +7,14 @@ import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 
+import Header from './Header';
+
 const App = () => {
     return (
-      <div>
+      <div className="ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
