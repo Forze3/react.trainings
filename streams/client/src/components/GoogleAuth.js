ee5270dbb5bb2e8d1d83c6b2e3de1d994534e99d
Elmae <manfersr@hotmail.com>
2019-09-22 21:11:22 -0500

    Started redux

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -11,24 +11,48 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+  
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
   
   renderAuthButton(){
     if (this.state.isSignedIn === null){
-      return <div>I dont know if we are signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      );
     } else{
-      return <div>I am not signed in</div>
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google 
+        </button>
+      );
     }
   }
 
   render(){
-    return <div>{this.renderAuthButton()}</div>;
+    return <div>{this.renderAuthButton()}</div>; 
   }
 }
 
 export default GoogleAuth;
+  
